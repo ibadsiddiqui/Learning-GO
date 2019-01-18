c103013af122d5aaad169bf5f4012c321b77ed78
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-01-18 11:46:47 +0500

    creating interfaces for structs and methods

---
+++ 12 - Interfaces/interface.go
@@ -0,0 +1,42 @@
+//  the main function, we create a variable a of type animal.
+// We assign a snake and a cat type to the animal and use Println to print a.description.
+// Since we have implemented the method describe in
+// both of the types (cat and snake) in a different way we get the description of the animal printed.
+package main
+
+import (
+	"fmt"
+)
+
+type animal interface {
+	description() string
+}
+
+type cat struct {
+	Type  string
+	Sound string
+}
+
+type snake struct {
+	Type      string
+	Poisonous bool
+}
+
+func (s snake) description() string {
+	return fmt.Sprintf("Poisonous: %v", s.Poisonous)
+}
+
+func (c cat) description() string {
+	return fmt.Sprintf("Sound: %v", c.Sound)
+}
+
+func main() {
+	var a animal
+	a = snake{Poisonous: true}
+	fmt.Println(a.description())
+	a = cat{Sound: "Meow!!!"}
+	fmt.Println(a.description())
+}
+
+//=> Poisonous: true
+//=> Sound: Meow!!!
