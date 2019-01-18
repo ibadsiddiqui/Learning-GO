9aa3150383f1e66b2a4d57cc406a668450dad5cf
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-01-18 14:06:26 +0500

    error handling added

---
+++ 14 - Error Handling/error.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	resp, err := http.Get("http://example.com/")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(resp)
+}
