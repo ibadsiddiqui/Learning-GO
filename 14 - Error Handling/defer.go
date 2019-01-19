b228f3e3943b6fa8b2c728c1412503c4335468f5
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-01-19 11:15:19 +0500

    added comments for defer and panic

--- 14 - Error Handling/defer.go
+++ 14 - Error Handling/defer.go
@@ -1,25 +1,44 @@
 // A defer statement pushes a function call onto a list.
 // The list of saved calls is executed after the surrounding function returns.
 // Defer is commonly used to simplify functions that perform various clean-up actions.
+
+// The function g takes the int i, and panics if i is greater than 3,
+// or else it calls itself with the argument i+1.
+// The function f defers a function that calls recover and prints the recovered
+// value (if it is non-nil). Try to picture what the output of this program might be before reading on.
+
 package main
 
 import "fmt"
 
 func main() {
+	// calls function f
 	f()
+
+	// prints after f() is executed
 	fmt.Println("Returned normally from f.")
 }
 
 func f() {
+
+	// creates a nested defer function which is called during panic
+	
 	defer func() {
+		// it captures the value at which panicking started
+		// and returns to main()
 		r := recover()
 		if r != nil {
 			fmt.Println("Recovered in f", r)
 		}
 	}()
+
+	// prints as during normal execution
 	fmt.Println("Calling g.")
+
+	// calls function g
+	
 	g(0)
-	fmt.Println("Returned normally from g.")
+	// g(5)
 }
 
 func g(i int) {
